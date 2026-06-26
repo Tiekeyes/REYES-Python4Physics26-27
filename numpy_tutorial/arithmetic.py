6817f926a74cd84558dad08afc176c495dc6c40c
Tiekeyes <sakakiharataiki@gmail.com>
2026-06-26 15:07:27 -0700

    added arithmetic file

--- numpy_tutorial/arithmetic.py
+++ numpy_tutorial/arithmetic.py
@@ -1,3 +1,14 @@
 import numpy as np
 
 #Scalar Arithmetic
+array = np.array([1, 2, 3])
+
+#add one to each element
+array += 1
+
+#raise each element to the power of 5
+array = array**5
+
+
+new_array = np.array([4, 9, 16])
+print(lambda x: int(x) (np.sqrt(new_array)))
