a5779e0e18c872dec4a15f41d9536b5a48969410
Tiekeyes <sakakiharataiki@gmail.com>
2026-06-27 12:53:06 -0700

    Finished Numpy Arithmetic

--- numpy_tutorial/arithmetic.py
+++ numpy_tutorial/arithmetic.py
@@ -10,5 +10,37 @@ array += 1
 array = array**5
 
 
-new_array = np.array([4, 9, 16])
-print(lambda x: int(x) (np.sqrt(new_array)))
+#Vectorized Arithmetic
+radii = np.array([1, 2, 3, 4])
+
+#square, then round each value
+print(np.round(np.sqrt(radii)))
+
+#calculating the area of each radius
+areas = np.round(np.pi * radii ** 2)
+print(areas)
+
+
+#Element-wise Arithmetic
+array1 = np.array([1, 2, 3])
+array2 = np.array([4, 5, 6])
+
+print(array1 + array2)
+print(array1 - array2)
+print(array1 * array2)
+print(array1 / array2)
+print(array1 ** array2)
+
+
+#Comparison Operators
+scores = np.array([91, 55, 100, 73, 82, 64])
+
+#find the student who got a perfect score
+print(scores == 100)
+
+#find students who scored more than 60
+print(scores > 60)
+
+#re-assigns any student who got less than 60 a fail (0)
+scores[scores < 60] = 0
+print(scores)
